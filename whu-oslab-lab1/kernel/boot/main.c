2d77d06ad196009032d5321d393e8e15f09f77eb
xsyWhu <3369916117@qq.com>
2025-09-27 20:42:18 +0800

    Update main.c

--- whu-oslab-lab1/kernel/boot/main.c
+++ whu-oslab-lab1/kernel/boot/main.c
@@ -8,7 +8,7 @@ int main()
     /* main 在这个实验中通常不会被调用（start 会直接进入），
        这个文件保留一个基本实现以防构建依赖。 */
     print_init();
-    printf("main reached on cpu %d\n", mycpuid());
+    puts("Hello OS from main\n");
     while (1) ;
     return 0;
 }
